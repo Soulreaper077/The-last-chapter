9e936013410df07c8d1c882ef0394af8c7d3cb71
Jesse <jesseponce233@gmail.com>
2022-08-04 21:46:36 -0500

    getting started with the routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const { route } = require('.');
+const { User, Book } = require('../../models');
+
+// this is to get all of the users 
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model:'' , // place model here
+                attributes: ['id', 'title' ]
+            },
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(require('./controllers/'));
+
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log( `Now listening on ${PORT}`));
 });
