b62f986bcf460f447f9c829c44539b6ce9e334f5
Jesse <jesseponce233@gmail.com>
2022-08-08 18:41:23 -0500

    getting the categories for books

--- controllers/api/book-routes.js
+++ controllers/api/book-routes.js
@@ -74,4 +74,23 @@ router.get('/book/:id', (req, res) => {
     });
 });
 
+// get a book by the category ? 
+router.get('/book/category', (req, res) => {
+    Book.findAll({
+        attibutes: [ 'categories' ],
+    })
+    .then(dbBookData => {
+        const books = dbBookData.map(book => book.get({ plain: true}));
+
+        res.render('homepage', {
+            books,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router; 
